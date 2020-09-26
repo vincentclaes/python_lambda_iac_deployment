38ac05385c5e093467931c1f66c3f08e5b093625
vincent <vclaes1986@gmail.com>
2020-09-26 17:33:48 +0200

    update project

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from python_lambda_iac_deployment.deployment.cdkworkshop_stack import CdkworkshopStack
+
+
+app = core.App()
+CdkworkshopStack(app, "deployment", env={'region': 'eu-central-1', 'account':"077590795309"})
+
+app.synth()
